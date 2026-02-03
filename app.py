65a163b68994874ffed0ca8046e5589da1a6d482
Maciej <maciek.widlak@gmail.com>
2026-02-03 12:16:44 +0100

    introduced two column split

--- app.py
+++ app.py
@@ -99,90 +99,92 @@ st.set_page_config(layout="wide")
 left_col, center_col, right_col = st.columns([1, 4, 1])
 
 with center_col:
-
     st.title("VIDEO/AUDIO SUMMARY")
     tab0, tab1 = st.tabs(["Upload file", "Parse YouTube video"])
     ### File upload option
     with tab0: 
         uploaded_file = st.file_uploader("Send a file for transcription", type=["mp3", "mp4", "wav", "mov", "ogg"], key="video_file")
-        if uploaded_file:
-            file_extension = uploaded_file.name.split(".")[-1].lower()  # Get file extension
-            file_bytes = uploaded_file.read()
-            file_bytes_md5 = md5(file_bytes).hexdigest()
-            # Recognize if the file is video or audio
-            if file_extension in ["mp3", "wav", "m4a", "ogg"]:
-                st.session_state["is_video"] = False
-            elif file_extension in ["mp4", "mov"]:
-                st.session_state["is_video"] = True
-            # on file change:
-            if st.session_state["file_bytes_md5"] != file_bytes_md5:
-                st.session_state["file_bytes_md5"] = file_bytes_md5
-                st.session_state["file_bytes"] = file_bytes
-                st.session_state["transcript"] = None
-                st.session_state["edtitable_text"] = None
-                st.session_state["audio_file_path"] = None
-                
-                # if file is video
-                if st.session_state["is_video"]: 
-                    st.video(st.session_state["file_bytes"], format="video/mp4")
-                    # Save uploaded video to a temporary file
-                    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_video_file:
-                        temp_video_file.write(file_bytes)
-                        temp_video_path = temp_video_file.name
-                    info_audio_placeholder = st.empty()
-                    info_audio_placeholder.info("Extracting audio, please wait...")
-                    # Convert video to audio
-                    audio = AudioSegment.from_file(temp_video_path, format="mp4")
-                    # Save audio to a temporary file
-                    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_audio_file:
-                        audio.export(temp_audio_file.name, format="mp3")
-                        st.session_state["audio_file_path"] = temp_audio_file.name  
-                        info_audio_placeholder.success("Audio was extracted.")
-                        st.write("Extracted audio:")
-                        st.audio(st.session_state["audio_file_path"], format="audio/mp3")
-                
-                else:  # if the file is audio
-                    st.audio(file_bytes, format=f"audio/{file_extension}")
-                    with tempfile.NamedTemporaryFile(delete=False, suffix=f".{file_extension}") as temp_audio_file:
-                        temp_audio_file.write(file_bytes)
-                        st.session_state["audio_file_path"] = temp_audio_file.name
-                    st.success("Audio file uploaded successfully. Ready for transcription.")
-
-            # Uploaded file didn't change
-            else:
-                if st.session_state["is_video"]:
-                    if st.session_state["transcript"]:
-                        st.video(st.session_state["file_bytes"], format="video/mp4", subtitles=st.session_state["transcript"]["srt"])
-                    else:
+        video_col, summary_col = st.columns(2, gap="small")
+        with video_col:
+            if uploaded_file:
+                file_extension = uploaded_file.name.split(".")[-1].lower()  # Get file extension
+                file_bytes = uploaded_file.read()
+                file_bytes_md5 = md5(file_bytes).hexdigest()
+                # Recognize if the file is video or audio
+                if file_extension in ["mp3", "wav", "m4a", "ogg"]:
+                    st.session_state["is_video"] = False
+                elif file_extension in ["mp4", "mov"]:
+                    st.session_state["is_video"] = True
+                # on file change:
+                if st.session_state["file_bytes_md5"] != file_bytes_md5:
+                    st.session_state["file_bytes_md5"] = file_bytes_md5
+                    st.session_state["file_bytes"] = file_bytes
+                    st.session_state["transcript"] = None
+                    st.session_state["edtitable_text"] = None
+                    st.session_state["audio_file_path"] = None
+                    
+                    # if file is video
+                    if st.session_state["is_video"]: 
                         st.video(st.session_state["file_bytes"], format="video/mp4")
-                    st.write("Audio:")
-                st.audio(st.session_state["audio_file_path"], format="audio/mp3")
-                
-            info_transcribe_placeholder = st.empty()
-            if st.session_state["transcript"] is None: 
-                if st.button("Start Transcription"):
-                    info_transcribe_placeholder.info("Transcribing audio... (this may take a while depending on the length of the audio)")  
-                    try:
-                        st.session_state["transcript"] = audio_utils.create_transcription(open(st.session_state["audio_file_path"], "rb").read(), get_openai_client()) 
-                    except AuthenticationError:
-                        info_transcribe_placeholder.error("Invalid API key. Please check your OpenAI API key and try again (refresh site).")
-                        st.stop()
-                    except Exception as e:
-                        info_transcribe_placeholder.error(f"An error occurred: {str(e)}")
-                        st.stop()
-                    # st.session_state["transcript"] = transcript
-                    st.rerun()
+                        # Save uploaded video to a temporary file
+                        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_video_file:
+                            temp_video_file.write(file_bytes)
+                            temp_video_path = temp_video_file.name
+                        info_audio_placeholder = st.empty()
+                        info_audio_placeholder.info("Extracting audio, please wait...")
+                        # Convert video to audio
+                        audio = AudioSegment.from_file(temp_video_path, format="mp4")
+                        # Save audio to a temporary file
+                        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_audio_file:
+                            audio.export(temp_audio_file.name, format="mp3")
+                            st.session_state["audio_file_path"] = temp_audio_file.name  
+                            info_audio_placeholder.success("Audio was extracted.")
+                            st.write("Extracted audio:")
+                            st.audio(st.session_state["audio_file_path"], format="audio/mp3")
+                    
+                    else:  # if the file is audio
+                        st.audio(file_bytes, format=f"audio/{file_extension}")
+                        with tempfile.NamedTemporaryFile(delete=False, suffix=f".{file_extension}") as temp_audio_file:
+                            temp_audio_file.write(file_bytes)
+                            st.session_state["audio_file_path"] = temp_audio_file.name
+                        st.success("Audio file uploaded successfully.")
 
+                # Uploaded file didn't change
+                else:
+                    if st.session_state["is_video"]:
+                        if st.session_state["transcript"]:
+                            st.video(st.session_state["file_bytes"], format="video/mp4", subtitles=st.session_state["transcript"]["srt"])
+                        else:
+                            st.video(st.session_state["file_bytes"], format="video/mp4")
+                        st.write("Audio:")
+                    st.audio(st.session_state["audio_file_path"], format="audio/mp3")
+                    
+                info_transcribe_placeholder = st.empty()
+                if st.session_state["transcript"] is None: 
+                    st.session_state["context"] = st.text_area("You can add additional context for the summary here")
+                    if st.button("Generate summary", key = "uploaded_file_btn" ):
+                        info_transcribe_placeholder.info("Transcribing audio... (this may take a while depending on the length of the audio)")  
+                        try:
+                            st.session_state["transcript"] = audio_utils.create_transcription(open(st.session_state["audio_file_path"], "rb").read(), get_openai_client()) 
+                        except AuthenticationError:
+                            info_transcribe_placeholder.error("Invalid API key. Please check your OpenAI API key and try again (refresh site).")
+                            st.stop()
+                        except Exception as e:
+                            info_transcribe_placeholder.error(f"An error occurred: {str(e)}")
+                            st.stop()
+                        # st.session_state["transcript"] = transcript
+                        st.rerun()
+        with summary_col:
             if st.session_state["transcript"]:
-                info_transcribe_placeholder.success("Transcription completed.")
-                st.session_state["context"] = st.text_area("You can add additional context for the summary here")
+                info_transcribe_placeholder.info("Transcription completed. Generating summary...")  
                 # transcription_text = parse_transcript(st.session_state["transcript"]) 
-                if st.button("Generate summary", key = "uploaded_file_btn" ):
-                    st.session_state["full_summary"] = ""
-                    placeholder = st.empty()
-                    for token in summarize_text(st.session_state["transcript"], st.session_state["context"]):
-                        st.session_state["full_summary"] += token
-                        placeholder.markdown(st.session_state["full_summary"])
+                # if st.button("Generate summary", key = "uploaded_file_btn" ):
+                st.session_state["full_summary"] = ""
+                placeholder = st.empty()
+                for token in summarize_text(st.session_state["transcript"], st.session_state["context"]):
+                    st.session_state["full_summary"] += token
+                    placeholder.markdown(st.session_state["full_summary"])
+                info_transcribe_placeholder.success("Transcription completed.")
 
     ### Youtube link option
     with tab1:
